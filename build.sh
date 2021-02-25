ee19f194129333d0e20a9883d8f333e4b7e8414b
nabhajit-ray <62326750+nabhajit-ray@users.noreply.github.com>
2021-02-25 18:29:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -127,11 +127,11 @@ if [ "$GITHUB_ACTIONS" = false ]; then
   exit_code_doc_generation=$?
 
 #Coveralls runs only when Travis is running the build
-else
-  echo -e "\n${COLOR_START}Running Coveralls${COLOR_END}"
-  coverage run --source=library/ -m pytest test/
-  coveralls
-  exit_code_coveralls=$?
+#else
+#  echo -e "\n${COLOR_START}Running Coveralls${COLOR_END}"
+#  coverage run --source=library/ -m pytest test/
+#  coveralls
+#  exit_code_coveralls=$?
 fi
 echo -e "\n${COLOR_START}Running tests${COLOR_END}"
 python -m pytest test/
