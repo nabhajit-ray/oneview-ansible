c12d4a0a97cc0a2bf5a73df422657232a0ac0602
nabhajit-ray <62326750+nabhajit-ray@users.noreply.github.com>
2021-02-25 14:27:57 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -121,7 +121,8 @@ else
 fi
 #Documentation is generated only in local builds
 
-#echo -e "\n${COLOR_START}Generating markdown documentation${COLOR_END}"
+echo "hi $GITHUB_ACTIONS"
+3echo -e "\n${COLOR_START}Generating markdown documentation${COLOR_END}"
 #build-doc/run-doc-generation.sh
 #exit_code_doc_generation=$?
 
